6c44257eec6a93a4a95014309b7ea81ead3f19ca
Juan Sebastian <jusegiraldo76@gmail.com>
2022-04-21 16:13:20 -0500

    arreglando responsive

--- src/components/PokeCards.js
+++ src/components/PokeCards.js
@@ -1,76 +1,75 @@
 import React, { useEffect, useState } from 'react'
-import { Card, Col} from 'react-bootstrap'
 import Swal from 'sweetalert2'
 
 
 const PokeCards = () => {
   const [cards, setCards] = useState([])
-  const [loadMore, setLoadMore] = useState('https://pokeapi.co/api/v2/pokemon?limit=50')
+  const [loadMore, setLoadMore] = useState('https://pokeapi.co/api/v2/pokemon?limit=52')
 
 
 
-const traerPokemon = async () => {
-  const res= await fetch(loadMore)
-  const data= await res.json() 
-  
-  setLoadMore(data.next)
+  const traerPokemon = async () => {
+    const res = await fetch(loadMore)
+    const data = await res.json()
 
-  function pokemonObject(result){
-    result.forEach(async pokemon =>{
-      const res= await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemon.name}`)
-      const data= await res.json()
-      setCards(list =>[...list, data])
-    })    
+    setLoadMore(data.next)
+
+    function pokemonObject(result) {
+      result.forEach(async pokemon => {
+        const res = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemon.name}`)
+        const data = await res.json()
+        setCards(list => [...list, data])
+      })
+    }
+    pokemonObject(data.results)
   }
-  pokemonObject(data.results)  
-}
 
-const activarModalPoke = (id) => {
-  cards.filter((mostrar) => (
-   mostrar.id === id      
- )) 
-}
+  const activarModalPoke = (id) => {
+    cards.filter((mostrar) => (
+      mostrar.id === id
+    ))
+  }
 
-useEffect(() => { 
-  traerPokemon()
-}, [])
+  useEffect(() => {
+    traerPokemon()
+  }, [])
 
-return (
-  
-  <div className='cardContainer'>   
-    {
-       cards.map((poke,index) => (                  
-              <Col key={index} onClick={() =>{activarModalPoke(Swal.fire({
+  return (
+
+    <div className='PokeCardPage'>
+      <div className='cardContainer'>
+
+        {
+          cards.map((poke, index) => (
+            <div key={index} onClick={() => {
+              activarModalPoke(Swal.fire({
                 title: `${poke.name}`,
+                content: `${poke.types}`,
                 width: 600,
                 imageUrl: `${poke.sprites.other.dream_world.front_default}`,
                 padding: '3em',
                 color: '#30a7d7',
                 background: '#fff url(/images/trees.png)',
-                confirmButtonColor:'#000',
+                confirmButtonColor: '#000',
                 backdrop: `
-                  rgba(0,0,0,0.8)
-                  url("https://img1.picmix.com/output/stamp/normal/0/9/0/4/1604090_a14a5.gif")
-                  left top
-                  no-repeat
-                `
-              }))}}>
-                <Card className='card'>
-                <Card.Text className='statsContainer' >                     
-                     <span className='statOne'>HP: {poke.stats[0].base_stat}</span>
-                     <span className='statTwo'>Attack: {poke.stats[1].base_stat}</span>                     
-                    </Card.Text>
-                  <Card.Img  variant="top" src={poke.sprites.other.dream_world.front_default} />
-                  <Card.Body>
-                    <Card.Title>{poke.name}</Card.Title>
-                    
-                  </Card.Body>
-                </Card>
-              </Col>          
-      ))
-    }
-  </div>
-)
+                rgba(0,0,0,0.8)
+                url("https://img1.picmix.com/output/stamp/normal/0/9/0/4/1604090_a14a5.gif")
+                left top
+                no-repeat
+              `
+              }))
+            }}>
+              <div className='pokeCard'>
+                <img alt='pokemon' src={poke.sprites.other.dream_world.front_default} />
+                <p> N° {poke.id}</p>
+                <h3>{poke.name}</h3>
+              </div>
+            </div>
+          ))
+        }
+      </div>
+    </div>
+  )
 }
 
 
